c92433485ac36f0ac68d76f2b0c6d1e8142eed89
Szilamer007 <ochuleta@gmail.com>
2022-11-03 20:07:26 +0100

    Add config page to the solution - this will be used to load the offline maps from the documents folder

---
+++ Rangeman/ConfigPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Rangeman
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}

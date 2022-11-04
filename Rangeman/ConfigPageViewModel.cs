da5cc0ed25eaf3c6365f347f3ecf7e109664c581
Szilamer007 <ochuleta@gmail.com>
2022-11-04 12:24:59 +0100

    Add first version of config viewmodel

---
+++ Rangeman/ConfigPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace Rangeman
+{
+    internal class ConfigPageViewModel : ViewModelBase
+    {
+        private bool useMbTilesChecked;
+        private ICommand applyCommand;
+
+        public bool UseMbTilesChecked { get => useMbTilesChecked; set { useMbTilesChecked = value; OnPropertyChanged("UseMbTilesChecked"); } }
+    }
+}
--- Rangeman/MyTabbedPageViewModel.cs
+++ Rangeman/MyTabbedPageViewModel.cs
@@ -1,25 +1,16 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Rangeman
+﻿namespace Rangeman
 {
     internal class MyTabbedPageViewModel
     {
         public MainPageViewModel MainPageViewModel { get; set; }
         public MapPageViewModel MapPageViewModel { get; set; }
+        public ConfigPageViewModel ConfigPageViewModel { get; set; }
 
         public MyTabbedPageViewModel(Android.Content.Context context)
         {
             MainPageViewModel = new MainPageViewModel(context);
             MapPageViewModel = new MapPageViewModel(context);
+            ConfigPageViewModel = new ConfigPageViewModel();
         }
     }
 }

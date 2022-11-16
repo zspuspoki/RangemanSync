7e6ef2abc665ed1288769e40463828e067a37090
Szilamer007 <ochuleta@gmail.com>
2022-11-16 15:27:17 +0100

    Add AppShellViewModel to make it possible to control enabled / disabled property of tabs

---
+++ Rangeman/AppShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Rangeman
+{
+    public class AppShellViewModel : ViewModelBase
+    {
+        private bool downloadPageIsEnabled;
+        private bool mapPageIsEnabled;
+        private bool configPageIsEnabled;
+
+        public bool DownloadPageIsEnabled { get => downloadPageIsEnabled; set { downloadPageIsEnabled = value; OnPropertyChanged("DownloadPageIsEnabled"); } }
+        public bool MapPageIsEnabled { get => mapPageIsEnabled; set { mapPageIsEnabled = value; OnPropertyChanged("MapPageIsEnabled"); } }
+        public bool ConfigPageIsEnabled { get => configPageIsEnabled; set { configPageIsEnabled = value; OnPropertyChanged("ConfigPageIsEnabled"); } }
+    }
+}

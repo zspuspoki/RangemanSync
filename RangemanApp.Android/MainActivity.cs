c9fcd42b8d854a5b926ff692f08967b3042b1977
Szilamer007 <ochuleta@gmail.com>
2022-11-19 15:24:39 +0100

    Add permission fix

--- RangemanApp.Android/MainActivity.cs
+++ RangemanApp.Android/MainActivity.cs
@@ -66,7 +66,7 @@ namespace employeeID.Droid
             alert.SetMessage(message);
             alert.SetPositiveButton("OK", (s, a) =>
             {
-                FinishAndRemoveTask();
+                Process.KillProcess(Process.MyPid());
                 return;
             });
 

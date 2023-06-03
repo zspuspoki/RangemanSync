8c64fb2f9dcc94cb0612f6e391b704c6ce0eb07a
Szilamer007 <ochuleta@gmail.com>
2023-06-03 13:04:49 +0200

    Add missing stuff to time related views and models including some basic validation and displaying changes

--- Rangeman/Views/Time/CustomTimeInfo.cs
+++ Rangeman/Views/Time/CustomTimeInfo.cs
@@ -1,12 +1,54 @@
-﻿namespace Rangeman.Views.Time
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rangeman.Views.Time
 {
+    public enum MonthType
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    public enum DayOfWeekType
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
     public class CustomTimeInfo
     {
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public int Day { get; set; }
-        public int Hour { get; set; }
-        public int Minute { get; set; }
-        public int Second { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a valid year between 2000 and 2200.")]
+        public int? Year { get; set; }
+
+        public MonthType Month { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a valid day of month (1-31)")]
+        public int? Day { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a valid hour (0-23)")]
+        public int? Hour { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a valid minute (0-59)")]
+        public int? Minute { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a valid second (0-59)")]
+        public int? Second { get; set; }
+
+        [Display(ShortName = "Day of week")]
+        public DayOfWeekType DayOfWeek { get; set; }
     }
 }
--- Rangeman/Views/Time/NTPTimeInfo.cs
+++ Rangeman/Views/Time/NTPTimeInfo.cs
@@ -1,7 +1,11 @@
-﻿namespace Rangeman.Views.Time
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Rangeman.Views.Time
 {
     public class NTPTimeInfo
     {
+        [Display(ShortName = "NTP server's URL")]
         public string NTPServer { get; set; }
     }
 }
